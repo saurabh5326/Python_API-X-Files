84d17d5bb2390cc52f8f56339ae10ef2feaa4a4e
Desayachi <saurabhmishra272@gmail.com>
2024-06-22 23:32:09 +0530

    Add files via upload

---
+++ flask api/tutorial61.py
@@ -0,0 +1,46 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "password"
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True  # <--- makes the permanent session
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login Succesfull!")
+        return redirect(url_for("user"))
+    else: 
+        if "user" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out!, {user}","info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)	
